cbd514b15e6be90d53e56935c45fd2f34acb2e48
FernandoGonza27 <fg760173@gmail.com>
2023-11-26 22:31:41 -0700

    terminado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,10 +45,7 @@
                     </svg>
                 </div>
 
-                <p class="text-gray-900 dark:text-white">
-                    Examen Final ISW811 - Jose Ali Rivas Gomez
-                </p>
-
+               
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     
                     <div class="grid grid-cols-1 md:grid-cols-2">
